1f607b1d3736db7a1ecd103b0d445d4dae446faa
linhangqi <lhq1025773894@gmail.com>
2026-05-05 11:09:44 +0800

    Fix local dev base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  base: "/twilight-hunter/",
+export default defineConfig(({ command }) => ({
+  base: command === "build" ? "/twilight-hunter/" : "/",
   plugins: [react()],
-});
+}));
